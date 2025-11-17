5888f88286bb274d6436d2315f1271ee5107911e
JuanDiego <juand.lopezm@autonoma.edu.co>
2025-11-17 18:40:25 -0500

    Se arreglo un problema dentro del admin dashboard que no cargaba el home, y tambien se soluciono el problema donde no renderizaba los estilos del boton de ajustar plan en el dashboard de oowner

--- app/(admin)/home.tsx
+++ app/(admin)/home.tsx
@@ -1,15 +1,6 @@
 import React from 'react';
-import ScreenHome from '../../modules/admin/home/ScreenHome';
-import { ScrollView, View } from 'react-native';
+import { ScreenHome } from '../../modules/admin/home/ScreenHomeAdmin';
 
 export default function AdminDashboard() {
-  <ScrollView 
-        contentContainerStyle={{ flexGrow: 1 }}
-        keyboardShouldPersistTaps="handled"
-        showsVerticalScrollIndicator={false}
-    >
-    <View className="flex-1 justify-center">            
-        <ScreenHome />
-    </View>
-  </ScrollView>
+  return <ScreenHome />;
 }
