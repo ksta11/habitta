4032fff74123593eba2600074707934342a24a34
Santiago Castaño <santicasta1003@gmail.com>
2025-11-21 22:40:20 -0500

    .

--- components/atoms/ButtonAtom.tsx
+++ components/atoms/ButtonAtom.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Pressable, Text, ActivityIndicator, View, ViewStyle } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import React from 'react';
+import { ActivityIndicator, Pressable, Text, View } from 'react-native';
 import { hapticFeedback } from '../../utils/haptics';
 
 interface ButtonAtomProps {
