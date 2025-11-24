642d7808255004d78620f828ede08d2295261980
Sebs <smasgg@gmail.com>
2025-11-24 12:41:41 -0500

    fix: Se soluciono las reviews para el usuario

--- app/(user)/(review)/[reviewId].tsx
+++ app/(user)/(review)/[reviewId].tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReviewForm from '../../../modules/reviews/ReviewForm';
 export default function UserReview() {
+    console.log('[UserReviewForm] Componente de entrada renderizado para user');
   return <ReviewForm />;
 }
