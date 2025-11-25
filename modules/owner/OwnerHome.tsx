af8cf6ecde2a4509ce631144e31fbee142116983
Sebs <smasgg@gmail.com>
2025-11-24 21:19:35 -0500

    fix: resolve conflicts

--- modules/owner/OwnerHome.tsx
+++ modules/owner/OwnerHome.tsx
@@ -4,17 +4,13 @@ import { ActivityIndicator, Pressable, ScrollView, Text, View } from 'react-nati
 import { useAuth } from "../../contexts/AuthContext";
 import { hapticFeedback } from '../../utils/haptics';
 import { pressedPrimaryButton, standarPrimaryButton } from '../../utils/TokensDesing';
-import { useReviewNavigation } from "../user/hooks";
 import { DashboardHeader } from "./dashboard/dashboard-header";
 import { QuickActions } from "./dashboard/quick-actions";
 import { RecentApplications } from "./dashboard/recent-applications";
 import { RevenueChart } from "./dashboard/revenue-chart";
 import { StatsGrid } from "./dashboard/stats-grid";
 import { useOwnerDashboard } from './hooks/useOwnerDashboard';
-import { hapticFeedback } from '../../utils/haptics';
-import { useAuth } from "../../contexts/AuthContext";
 import { useReviewNavigation } from "../user/hooks/useReviewNavigation";
-import { standarPrimaryButton, pressedPrimaryButton } from '../../utils/TokensDesing';
 
 
 export default function Dashboard() {
